4b6415260c03dab45155d6e95b2c92ffb096a7d3
omkharche <omkharche321@gmail.com>
2023-04-16 23:59:25 +0530

    working

--- to-do-list/src/MyComponent/Footer.js
+++ to-do-list/src/MyComponent/Footer.js
@@ -11,3 +11,16 @@ export default function Footer({}) {
     </>
   )
 }
+import React from 'react'
+export default function Footer({}) {
+
+    return (
+    <>
+        <footer className="bg-dark text-light py-3" style={{position:'absolute', top:'96vh', width:'100%'}}>
+            <p className="text-center" style={{margin:'0px'}}>
+                Copyright &copy; MyTodosList.com
+            </p>
+        </footer>
+    </>
+  )
+}
