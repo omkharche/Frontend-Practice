030d546df819e87ee8585763cc005e3588761ad4
omkharche <omkharche321@gmail.com>
2023-04-19 23:57:40 +0530

    working

--- to-do-list/src/MyComponent/MyTodo.js
+++ to-do-list/src/MyComponent/MyTodo.js
@@ -15,3 +15,20 @@ export default function MyTodo({todos, onDelete}) {
     </>
   )
 }
+import React from 'react'
+
+export default function MyTodo({todos, onDelete}) {
+  return (
+    <>
+        <div className="container-fluid" style={{paddingLeft:'0px'}}>
+            <br/>
+            <h6>{todos.sno}</h6>
+            <h5>{todos.title}</h5>
+            <p>{todos.desc}</p>
+            <button className="btn btn-danger btn-sm " onClick={()=>{onDelete(todos)}}>
+                Delete
+            </button>
+        </div>      
+    </>
+  )
+}
