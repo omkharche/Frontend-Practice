938b8d253280e18228af2baa04e77c85b4e2f8d3
omkharche <omkharche321@gmail.com>
2023-05-05 22:59:46 +0530

    working

--- cw-technologies/src/Components/Home.js
+++ cw-technologies/src/Components/Home.js
@@ -1,28 +1,21 @@
 import React from 'react'
 import ub1 from './Assets/ub1.png'
 import "./CSS/Home.css"
+
 export default function Home() {
   return (
     <>
         <div className='container-fluid hc1'>
-            <div>
-                <p id='s1'>We Design Digital platforms and brands with Future Trends</p>
-                <p className='s2'><span className='auto-type'>Hello</span></p>
+            <div class='d1'>
+                <p id='s1'>We Design Digital platforms and brands with Future Trends 🚀 </p>
+                <p className='s2'><span className='auto-type'>Designing with future trends creates innovative and engaging digital experiences. Requires understanding of market trends, user needs, and emerging technologies. Adaptability, flexibility, and innovation are key.</span></p>
+                <input type="button" className='btn1' value="Explore More" />
             </div>
-            <div>
+            <div class='d2'>
                 <img src={ub1} class="cs1" alt="guy on laptop"></img>
             </div>
         </div>
-
-        <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
-        <script>
-            var typed = new Typed('.auto-type', (
-             strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-            typeSpeed: 50,
-            ));
-
-        </script>
-
+        
     </>
   )
 }
