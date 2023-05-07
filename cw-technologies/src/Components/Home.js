92626f63c2d24ded3b9cacc3046087d436f224d7
omkharche <omkharche321@gmail.com>
2023-05-07 23:57:03 +0530

    working

--- cw-technologies/src/Components/Home.js
+++ cw-technologies/src/Components/Home.js
@@ -1,8 +1,6 @@
 import React from 'react'
-
 import ub1 from './Assets/ub1.png'
 import "./CSS/Home.css"
-
 export default function Home() {
   return (
     <>
@@ -15,10 +13,122 @@ export default function Home() {
             <div class='d2'>
                 <img src={ub1} class="cs1" alt="guy on laptop"></img>
             </div>
-        </div>  
+        </div>
+
+        <div className='icon-div'>
+          <div className="line-1"></div>
+          <i class="fa-solid fa-angles-down fa-bounce icon1-css"></i>
+          <div className="line-2"></div>
+        </div>
+        <div className="s-div">
+          <span className='s1'>Welcome to CW Technology Services</span><br></br>
+          <span className='sp2'>Business Secrets for your Success</span><br></br>
+          <p className='s3'>AnkHub Services Technology  is an<span className="sp4"> innovative team </span>who would be providing services to its client <span className="sp4">globally,</span> promoted by <span className="sp4">professionals</span> having rich expression in <span className="sp4">business intelligent, consulting,  staffing, outsourcing,</span> and <span className="sp4">training domain</span> specializes in the Business of <span className="sp4">infrastructure services</span> and <span className="sp4">talent management solutions</span>.</p>
+        </div>
         
-        <i class="fa-solid fa-chevrons-down"></i>
-      
+        <div className='c-div'>
+          <div className='cd1'>
+              <img className='cimg1' src='https://ankhub.in/wp-content/uploads/2023/02/Inudstry.png' alt="img1"></img>  
+              <p className='cp1'>Industry Experts</p>
+              <p className='cp2'>Get trained from our industry experts.</p><br></br><br></br>
+          </div>
+          <div className='cd1'>
+              <img className='cimg1' src='https://ankhub.in/wp-content/uploads/2023/02/Inudstry1.png' alt="img1"></img>  
+              <p className='cp1'>Affordable Courses</p>
+              <p className='cp2'>From students to professionals we provide affordable courses</p><br></br>
+        </div>
+          <div className='cd1'>
+              <img className='cimg1' src='https://ankhub.in/wp-content/uploads/2023/02/Inudstry2.png' alt="img1"></img>  
+              <p className='cp1'>Efficient & Flexible​</p>
+              <p className='cp2'>Efficient product handling and flexible timings.</p><br></br>
+          </div>
+        </div>
+
+        <div className='t-div'>
+          <br></br>
+            <p className='ts1'>Technologies We Work...</p>
+            <div className='tflex'>
+              <div className='tf1'>
+                <img className='timg' src="https://ankhub.in/wp-content/uploads/2022/09/New-App-Devleopment.png" alt="timg"></img>
+                <br></br><div className='tp1'>App Development</div>
+              </div>
+              <div className='tf1'>
+                <img className='timg' src="https://ankhub.in/wp-content/uploads/2022/09/New-Wordpress.png" alt="timg"></img>
+                <br></br><div className='tp1'>WordPress Development</div>
+              </div>
+              <div className='tf1'>
+                <img className='timg' src="https://ankhub.in/wp-content/uploads/2022/09/New-java.png" alt="timg"></img>
+                <br></br><div className='tp1'>Java Development</div>
+              </div>
+              <div className='tf1'>
+                <img className='timg' src="https://ankhub.in/wp-content/uploads/2022/09/New-Networking.png" alt="timg"></img>
+                <br></br><div className='tp1'>Networking</div>
+              </div>
+              <div className='tf1'>
+                <img className='timg' src="https://ankhub.in/wp-content/uploads/2022/09/new-Cloud.png" alt="timg"></img>
+                <br></br><div className='tp1'>Cloud Development</div>
+              </div>
+              <div className='tf1'>
+                <img className='timg' src="https://ankhub.in/wp-content/uploads/2022/09/new-Linux.png" alt="timg"></img>
+                <br></br><div className='tp1'>Linux</div>
+              </div>
+              <div className='tf1'>
+                <img className='timg' src="https://ankhub.in/wp-content/uploads/2022/09/New-Digital-Marketing.png" alt="timg"></img>
+                <br></br><div className='tp1'>Digital Marketing</div>
+              </div>
+              <div className='tf1'>
+                <img className='timg' src="https://ankhub.in/wp-content/uploads/2022/09/new-Php.png" alt="timg"></img>
+                <br></br><div className='tp1'>PHP Development</div>
+              </div>
+            </div>
+            <br></br>
+            </div>
+            
+
+            <div className='j'>
+              <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <div className="cwh">
+                      <img src="https://ankhub.in/wp-content/uploads/2022/08/Adarsh-Learning-150x150.png" class="d-block w-100 " alt="..." />
+                      <img src="https://ankhub.in/wp-content/uploads/2022/08/ambience-garden-150x150.png" class="d-block w-100" alt="."></img>
+                      <img src="https://ankhub.in/wp-content/uploads/2022/08/ambience-garden-150x150.png" class="d-block w-100" alt="."></img>
+                      <img src="https://ankhub.in/wp-content/uploads/2022/08/ambience-garden-150x150.png" class="d-block w-100" alt="."></img>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://ankhub.in/wp-content/uploads/2022/08/ambience-garden-150x150.png" class="d-block w-30" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://ankhub.in/wp-content/uploads/2022/08/chaatrapati-digimark-150x150.png" class="d-block w-30" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://ankhub.in/wp-content/uploads/2022/08/ambience-garden-150x150.png" class="d-block w-30" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://ankhub.in/wp-content/uploads/2022/08/chaatrapati-digimark-150x150.png" class="d-block w-30" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://ankhub.in/wp-content/uploads/2022/08/ambience-garden-150x150.png" class="d-block w-30" alt="..." />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://ankhub.in/wp-content/uploads/2022/08/chaatrapati-digimark-150x150.png" class="d-block w-30" alt="..." />
+                  </div>
+                  
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+            <br></br>
+       
+        
       </>
   )
 }
