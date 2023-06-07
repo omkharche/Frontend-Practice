657f48d82346181bf6c1bf4360a28438dd6e8098
omkharche <omkharche321@gmail.com>
2023-06-08 02:32:22 +0530

    desktop view done

--- cw-technologies/src/Components/Home.js
+++ cw-technologies/src/Components/Home.js
@@ -133,32 +133,117 @@ const btnprev = () => {
                     <button className='btn12'>Submit</button>
                   </div>
                 </div>
-                <div className="footer-details">
-                  <div className='row'>
-                    <div className='col'>           
-                      Quick Links
-                    </div>
+                <div className="footer-details container">
+                  <div className='row1'>
+                    
                     <div className='co1'>
-                    Useful Links
+                      <ul className='list1'>
+                        <li className='fh'>
+                          Quick Links
+                        </li>
+                        <li className='fd'>
+                          Services
+                        </li>
+                        <li className='fd'>
+                          Privacy Policy
+                        </li>
+                        <li className='fd'>
+                          Internships
+                        </li>
+                        <li className='fd'>
+                          Blogs
+                        </li>
+                      </ul>
 
                     </div>
+                  </div>
+                  <div className='row1'>
                     <div className='co1'>
-                      
+                      <ul className='list1'>
+                        <li className='fh'>
+                          Useful Links
+                        </li>
+                        <li className='fd'>
+                          Contact Us
+                        </li>
+                        <li className='fd'>
+                          About Us
+                        </li>
+                        <li className='fd'>
+                          Home
+                        </li>
+                        <li className='fd'>
+                          Career
+                        </li>
+                      </ul>
+
                     </div>
+                  </div>
+                  <div className='row'>
                     <div className='co1'>
-                      
+                      <ul className='list1'>
+                        <li className='fh'>
+                          Working Hours
+                        </li>
+                        <li className='fd'>
+                          10:30am – 8:00pm
+                        </li>
+                        <li className='fd'>
+                          Monday to Saturday
+                        </li>
+                        <li className='fd'>
+                          The Company is Closed on Sundays
+                        </li>
+                      </ul>
+
                     </div>
+                  </div>
+                  <div className='row'>
                     <div className='co1'>
-                      
+                      <ul className='list1'>
+                        <li className='fh'>
+                          Contact Us
+                        </li>
+                        <li className='fd'>
+                          +91-9021070048
+                        </li>
+                        <li className='fd'>
+                          info@cwtech.in
+                        </li>
+                        <li className='fd'>
+                          Hinjewadi Phase 3, Pune, India
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='co1'>
+                      <ul className='list1'>
+                        <li className='fh'>
+                          Follow
+                        </li>
+                        <li className='fd1'>
+                        <i class="fa-brands fa-instagram"></i>&nbsp;&nbsp;&nbsp;
+                        <i class="fa-brands fa-facebook"></i>
+                        </li>
+                        <li className='fd1'>
+                        <i class="fa-brands fa-twitter"></i>&nbsp;&nbsp;
+                        <i class="fa-brands fa-youtube"></i>
+                        </li>
+                        <li className='fd1'>
+                        <i class="fa-brands fa-linkedin-in"></i>
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </div>
-
-                 
+                <div className='line'></div>
+                <p className='rights'>2023 All rights are reserved @CW_Technologies</p>
+                <br></br>
               </div>
             </footer>
 
-      </>
+      </> 
   )
   
 }
